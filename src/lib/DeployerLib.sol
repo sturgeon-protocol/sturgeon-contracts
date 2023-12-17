254631cf98cedba3f4eb3984b20c707e10e899a4
Alien Deployer <m@a17.name>
2023-12-17 16:41:03 +0300

    lint

--- src/lib/DeployerLib.sol
+++ src/lib/DeployerLib.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.8.21;
 import "../HarvesterVault.sol";
 
 library DeployerLib {
-    function deployHarvesterVault(address controller, address underlying, string calldata vaultName, string calldata vaultSymbol) external returns(address) {
+    function deployHarvesterVault(
+        address controller,
+        address underlying,
+        string calldata vaultName,
+        string calldata vaultSymbol
+    ) external returns (address) {
         return address(new HarvesterVault(controller, IERC20(underlying), vaultName, vaultSymbol, 4_000));
     }
 }
