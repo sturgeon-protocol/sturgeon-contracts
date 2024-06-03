87c213b068f38c7cc560181f39e5af914c21c7d8
Alien Deployer <m@a17.name>
2024-06-03 17:59:13 +0300

    refactor vesting

--- src/interfaces/IVesting.sol
+++ src/interfaces/IVesting.sol
@@ -1,6 +1,27 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.21;
 
+/// @title Vesting contract interface
 interface IVesting {
-    function start(uint amount) external;
+
+    /// @dev Token for vesting
+    function token() external view returns(address);
+
+    /// @dev Period that will start after the cliff
+    function vestingPeriod() external view returns(uint);
+
+    /// @dev Delay before the vesting
+    function cliffPeriod() external view returns(uint);
+
+    /// @dev Who will receive the tokens
+    function claimant() external view returns(address);
+
+    /// @dev Delay before the vesting
+    function startTs() external view returns(uint);
+
+    /// @dev Amount to distribute during vesting period
+    function toDistribute() external view returns(uint);
+
+    /// @dev Claim tokens
+    function claim() external returns(uint amount);
 }
